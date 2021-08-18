36e7f52aafcc19a1988bbeac97ba9af1b956d165
Clay Dowling <clayton.dowling@dana.com>
2021-08-18 12:53:55 -0400

    Can read basic requirements

--- validatetests/requirement.go
+++ validatetests/requirement.go
@@ -1,18 +1,39 @@
 package main
 
-import "container/list"
+import (
+	"container/list"
+	"encoding/csv"
+	"io"
+	"strings"
+)
 
 type RequirementItem struct {
-	Id string
+	Id          string
 	Description string
 }
 
 // Requirements contains the list of formal requirements and associated tests
 var Requirements = list.New()
 
-// LoadRequirements reads a single CSV file with requirements into memory
+// ParseRequirements reads a single CSV file with requirements into memory
 // and returns associated RequirementItems
-func LoadRequirements(contents []byte, idcolumn int, descriptioncolumn int) ([]RequirementItem, error) {
+func ParseRequirements(src io.Reader, idcolumn int, descriptioncolumn int) ([]RequirementItem, error) {
 
-	return []RequirementItem{}, nil
+	reader := csv.NewReader(src)
+	contents, err := reader.ReadAll()
+	if err != nil {
+		return []RequirementItem{}, err
+	}
+
+	var items = []RequirementItem{}
+
+	for i := 0; i < len(contents); i++ {
+		item := RequirementItem{
+			Id:          strings.TrimSpace(contents[i][idcolumn]),
+			Description: strings.TrimSpace(contents[i][descriptioncolumn]),
+		}
+		items = append(items, item)
+	}
+
+	return items, nil
 }
--- validatetests/requirement_test.go
+++ validatetests/requirement_test.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/assertgo/assert"
+)
+
+func TestParseRequirements_GivenIdAndDescriptionColumn_ReturnsIdAndDescription(t *testing.T) {
+	var content = `ID.1,  First Requirement
+ ID.2,"Second ""Requirement"""
+`
+
+	actual, err := ParseRequirements(strings.NewReader(content), 0, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert := assert.New(t)
+	assert.ThatInt(len(actual)).IsEqualTo(2)
+	assert.ThatString(actual[0].Id).IsEqualTo("ID.1")
+	assert.ThatString(actual[0].Description).IsEqualTo("First Requirement")
+	assert.ThatString(actual[1].Id).IsEqualTo("ID.2")
+	assert.ThatString(actual[1].Description).IsEqualTo(`Second "Requirement"`)
+}
